7c184d91cf15843a050e64bd5f37cbbcad2b860e
josue <jcalderonv2@ucenfotec.ac.cr>
2018-06-15 16:42:01 -0600

    Se crea la clase Naipe que contiene 52 cartas.

---
+++ src/primerExamen/objetos/Naipe.java
@@ -0,0 +1,34 @@
+package primerExamen.objetos;
+
+import java.util.ArrayList;
+
+import primerExamen.gestor.Cl;
+
+public class Naipe {
+
+	public ArrayList<Cl> crearDeck() throws Exception {
+		ArrayList<Cl> naipe = new ArrayList<Cl>();
+
+		Cl newCard = new Cl();
+
+		Nombre nombres[] = Nombre.values();
+		Palo palos[] = Palo.values();
+
+		Nombre nombreTemporal;
+		Palo paloTemporal;
+
+		for (int i = 0; i < palos.length; i++) {
+			for (int j = 0; j < nombres.length; j++) {
+
+				nombreTemporal = nombres[j];
+				paloTemporal = palos[i];
+
+				newCard.nuevaCarta(nombreTemporal, paloTemporal);
+				naipe.add(newCard);
+
+			}
+		}
+		return naipe;
+	}
+
+}
--- src/primerExamen/test/primerExamenPruebas.java
+++ src/primerExamen/test/primerExamenPruebas.java
@@ -14,7 +14,7 @@ public class primerExamenPruebas {
 	@Test
 	public void testValores() throws Exception {
 
-		assertEquals(1, valor.crearCarta(Nombre.As, Palo.Escudos, 1).getValor());
+		assertEquals(1, valor.nuevaCarta(Nombre.As, Palo.Escudos).getValor());
 
 	}
 
@@ -23,9 +23,18 @@ public class primerExamenPruebas {
 
 		Carta crearCarta = new Carta(Nombre.As, Palo.Gotas, 1);
 
-		assertEquals(crearCarta.getValor(), valor.crearCarta(Nombre.As, Palo.Escudos, 1).getValor());
-		assertEquals(crearCarta.getNombre(), valor.crearCarta(Nombre.As, Palo.Flores, 1).getNombre());
+		assertEquals(crearCarta.getValor(), valor.nuevaCarta(Nombre.As, Palo.Gotas).getValor());
+		assertEquals(crearCarta.getNombre(), valor.nuevaCarta(Nombre.As, Palo.Flores).getNombre());
 
+	}
+
+	@Test
+	public void testNaipes() throws Exception {
+
+		int tamanno = 52;
+		Naipe deck = new Naipe();
+
+		assertEquals(tamanno, deck.crearDeck().size());
 	}
 
 }
